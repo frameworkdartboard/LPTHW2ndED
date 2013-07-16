6f722a3fee88a5f395a74410f1a163bc9d2552b9
erickramer3000 <eric.kramer@gmail.com>
2013-07-16 08:04:56 -0400

    adding back web testing code

---
+++ gothonweb/tests/app_tests.py
@@ -0,0 +1,22 @@
+from nose.tools import *
+from bin.app import app
+from tests.tools import assert_response
+
+def test_index():
+    # check that we get a 404 on the / URL
+    resp = app.request("/")
+    assert_response(resp, status='303 See Other')
+    assert_response(resp, status='303 See Other', contains='Gothons from Planet Percal')
+
+    # test our first GET request to /hello
+#    resp = app.request("/game")
+#    assert_response(resp)
+
+    # test if 
+#    resp = app.request("/hello", method="POST")
+#    assert_response(resp, contains="Nobody")
+#
+#    # test that we get expected values
+#    data = {'name': 'Zed', 'greet': 'Hola'}
+#    resp = app.request("/hello", method="POST", data=data)
+#    assert_response(resp, contains="Zed")
